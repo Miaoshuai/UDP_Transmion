b9dcb8bc9284232336fc9ac6098db74e09b1909a
miaoshua <945970809@qq.com>
2016-01-21 13:45:46 +0800

    接受文件类的定义

--- recv_file.h
+++ recv_file.h
@@ -30,6 +30,9 @@ class RecvFile
         DataPacketSet       recvDataSet_;       //接收数据的一个buffer用来将数据弄为有序
         sockaddr_in         recvAddress_;       //接收方地址
         sockaddr_in         sendAddress_;       //发送发地址
+        int                 recvFd_             //接受端套接字描述符
+
+        int                 udpPort_;           //与服务器所发地址的端口一致
 };
 
 
